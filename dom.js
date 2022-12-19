e8557f90927801669f6f5efee4804423da27f232
JulianToppo <juliantoppo95@gmail.com>
2022-12-19 19:13:26 +0530

    Making the 3rd list in the item of green background and every list member have bold characters

--- dom.js
+++ dom.js
@@ -32,7 +32,28 @@
 // console.log(items[1])
 // items[1].textContent= "Hello 2"
 
-var item=document.getElementsByClassName('title')
-console.log(item)
-item[0].style.color="Green"
-item[0].style.fontWeight="bold"
+//Changing the color of add items green and making it bold
+// var item=document.getElementsByClassName('title')
+// console.log(item)
+// item[0].style.color="Green"
+// item[0].style.fontWeight="bold"
+
+var item=document.getElementsByClassName('list-group-item')
+// console.log(item.length)
+// console.log(item[1])
+// item[1].textContent="Hello"
+// item[1].style.fontWeight="bold"
+// item[1].style.backgroundColor="Yellow"
+
+for(var i=0;i<item.length;i++){
+    item[i].style.backgroundColor="#f4f4f4" //making the background color light grey
+}
+
+item[2].style.backgroundColor="Green"
+
+for(var i=0;i<item.length;i++){
+    item[i].style.fontWeight="bold"
+}
+
+
+
