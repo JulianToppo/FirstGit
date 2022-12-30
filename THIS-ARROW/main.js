2acc58fb38220aa0c16bd059f65bf77a50e66f2b
JulianToppo <juliantoppo95@gmail.com>
2022-12-30 23:02:37 +0530

    eligible for placements with arrow function and it's check

--- THIS-ARROW/main.js
+++ THIS-ARROW/main.js
@@ -22,6 +22,16 @@ class Student{
     static totalstudents(){
         console.log(Student.count);
     }
+
+    eligibleForPlacements=(age,minimarks) =>{
+
+        if(this.age>age && this.marks>minimarks){
+            console.log(`${this.name} is eligible for placements`);
+        }else{
+            console.log(`${this.name} is not eligible for placements`);
+        }
+
+    }
 }
 
 const chintu=new Student('chinti',20,1344332,80);
@@ -30,5 +40,12 @@ const jessica=new Student('jessica',20,43342,30);
 const monika=new Student('monika',20,53534,80);
 const rachel=new Student('rachel',20,534,90);
 
+chintu.eligibleForPlacements(15,67);
+mintu.eligibleForPlacements(15,67);
+jessica.eligibleForPlacements(15,67);
+monika.eligibleForPlacements(15,67);
+rachel.eligibleForPlacements(15,67);
+
+// jessica.isEligible()
 
 Student.totalstudents();
