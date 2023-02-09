0e338fe3eab5933e941e9d605f683b6228d95254
JulianToppo <juliantoppo95@gmail.com>
2023-02-09 16:04:52 +0530

    Using middleware for different routes and parsing the response sent by the form

--- NodeJS/Server/app.js
+++ NodeJS/Server/app.js
@@ -1,14 +1,23 @@
 const express=require('express');
 
+const bodyParser=require('body-parser');
+
 const app=express();
 
-app.use((req,res,next) =>{
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product',(req,res,next) =>{
     console.log("In the middleware");
-    next();
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form></body></html>');
 })
 
+app.use("/product",(req,res,next)=>{
+
+    console.log(req.body);
+    res.redirect("/");
+})
 //Sequence of the middleware function is strictly req,res and next
-app.use((req,res,next) =>{
+app.use('/',(req,res,next) =>{
     console.log('In another middleware');
     res.send('<h1>Hello From Express!</h1>');
 });
