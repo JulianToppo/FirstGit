2edff5451853369a3ce45f1214b8094e547c7cc1
JulianToppo <juliantoppo95@gmail.com>
2023-02-10 20:20:54 +0530

    Contact Page,success page and linking them to their respective css

--- NodeJS/Server/app.js
+++ NodeJS/Server/app.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use('/admin',adminRoute);
 app.use('/shop',shopRoute);
 
-console.log(path.join(__dirname,"NodeJS","Server","public"));
-app.use(express.static(path.join(__dirname,"NodeJS","Server","public")));
+
+app.use(express.static(path.join(__dirname,"..","..","NodeJS","Server","public")));
 //Status when page doesn't finds it's route
 app.use((req,res,next)=>{
    // res.status(404).send('<h1>Page not found</h1>');
--- NodeJS/Server/routes/admin.js
+++ NodeJS/Server/routes/admin.js
@@ -16,4 +16,16 @@ router.post("/add-product",(req,res,next)=>{
     res.redirect("/shop");
 })
 
+router.get('/contactus',(req,res,next) =>{
+    console.log("In the middleware product admin");
+   // res.send('<html><body><form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form></body></html>');
+    res.sendFile(path.join(rootDir,'views','contactus.html'));
+})
+
+router.post("/success",(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','success.html'));
+})
+
+
+
 module.exports=router
