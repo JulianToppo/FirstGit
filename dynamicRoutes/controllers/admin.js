628bdf7fdb0863d688bff7c56b7400221f71e12b
JulianToppo <juliantoppo95@gmail.com>
2023-02-12 17:09:47 +0530

    Edit and Delete functinality for the product

--- dynamicRoutes/controllers/admin.js
+++ dynamicRoutes/controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', { //navigate view
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -15,9 +15,47 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.postEditProduct = (req,res,next)=>{
+  const prodId = req.body.productId;
+  const updatedtitle= req.body.title;
+  const updatedprice= req.body.price;
+  const updatedImageUrl= req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedtitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedprice);
+
+  updatedProduct.save();
+  res.redirect('/admin/products')
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+  const prodId=req.params.productId;
+  Product.findById(prodId,product => {
+    if(!product){
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', { //navigate view
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product', //highlight
+    editing: editMode ,
+    product: product,
+  
+  });
+  })
+  
 };
 
 exports.getProducts = (req, res, next) => {
@@ -28,4 +66,11 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
+};
+
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
