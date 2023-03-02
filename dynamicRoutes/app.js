db8dd29183b6655d8ceca8cb9c5431ba9c570c96
JulianToppo <juliantoppo95@gmail.com>
2023-03-02 13:11:41 +0530

    oneMay and belongsto implementation with sequelise operations

--- dynamicRoutes/app.js
+++ dynamicRoutes/app.js
@@ -7,11 +7,15 @@ const errorController = require('./controllers/error');
 const sequelize= require('./util/database');
 const app = express();
 
+const User= require('./models/user');
+const Products= require('./models/product');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product = require('./models/product');
 
 // db.execute('SELECT * FROM products')
 // .then(result=>{
@@ -23,16 +27,44 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+     .then(user => {
+       req.user = user;
+       console.log(req.user);
+       next();
+     })
+     .catch(err => console.log("julian",err));
+ });
+ 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-   // console.log(result);
+
+Product.belongsTo(User,{ constraints: true, onDelete:'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
- }).catch(err => {
+  })
+  .catch(err => {
     console.log(err);
-})
+  });
 
 
