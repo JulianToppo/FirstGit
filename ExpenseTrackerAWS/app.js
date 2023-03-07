fe63e25225774a09f1ebf4d8233b2e9091017374
JulianToppo <juliantoppo95@gmail.com>
2023-03-07 15:48:06 +0530

    addition of razorpay and premium user subscription

--- ExpenseTrackerAWS/app.js
+++ ExpenseTrackerAWS/app.js
@@ -1,14 +1,19 @@
 
-
 const express= require('express');
 const loginSignUpRoutes= require('./routes/loginsignup');
 const expenseRoutes= require('./routes/expense');
+const purchaseRoutes = require('./routes/purchasePremium')
 const path=require('path');
 const bodyParser=require('body-parser')
 const sequelize=require('./util/database')
 const user= require('./model/user');
 const expense= require('./model/expense');
+const order= require('./model/order');
 const app=express();
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
 
 
 app.use(express.static(path.join(__dirname,'public')))
@@ -17,8 +22,12 @@ app.use(bodyParser.json({ extended: false }));
 user.hasMany(expense);
 expense.belongsTo(user);
 
+user.hasMany(order);
+order.belongsTo(user);
+
 app.use('/',loginSignUpRoutes);
 app.use('/expense',expenseRoutes);
+app.use('/purchase',purchaseRoutes);
 //sql sync 
 sequelize.sync().then(result => {
     // console.log(result);
