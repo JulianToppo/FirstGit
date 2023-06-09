f5d5f1910871a3e4de64445234113ca7dfd11c1e
JulianToppo <juliantoppo95@gmail.com>
2023-06-09 15:37:10 +0530

    Creating User table with save and fetchbyId functino

--- MongoDBPractice/models/user.js
+++ MongoDBPractice/models/user.js
@@ -1,16 +1,43 @@
-const Sequelize = require('sequelize');
 
-const sequelize = require('../util/database');
+const getDb = require('../util/database').getDb
+const ObjectId= require('mongodb').ObjectId;
+class User{
+  constructor(name,email){
+    this.name=name;
+    this.email=email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save(){
+    const db=getDb;
+    return db.collection('users')
+    .insertOne(this)
+    .then(result=>{
+      console.log(result);
+      return result;
+    })
+  }
+
+  static findUserById(userId){
+    const db=getDb;
+    return db.collection('users')
+    .findOne({_id:new ObjectId(userId)}); //next is used to return the single element 
+  }
+}
+
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = require('../util/database');
+
+// const User = sequelize.define('user', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   name: Sequelize.STRING,
+//   email: Sequelize.STRING
+// });
 
 module.exports = User;
